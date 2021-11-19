0fa0ffad39a170af7466b71f39eb18066726d3ca
sMenaka <dgsmenaka@gmail.com>
2021-11-19 10:12:56 +0530

    Initial commit

---
+++ NoSQL-POC/src/main/java/com/poc/NoSQL/POC/NoSqlPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.NoSQL.POC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoSqlPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoSqlPocApplication.class, args);
+	}
+
+}
---
+++ SQL/src/main/java/com/poc/demo/SqlPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlPocApplication.class, args);
+	}
+
+}
